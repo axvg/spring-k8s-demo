fa9b241ac89faf7c326b38cda5cf77461fead3a7
Alex Vega <axvg@users.noreply.github.com>
2026-02-13 20:12:41 -0500

    update controllers with configmap

--- src/main/java/com/axvg/spring_k8s_demo/controllers/K8sController.java
+++ src/main/java/com/axvg/spring_k8s_demo/controllers/K8sController.java
@@ -1,5 +1,6 @@
 package com.axvg.spring_k8s_demo.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,13 +14,23 @@ public class K8sController {
 
     private int contador = 0;
 
+    @Value("${APP_NAME:default}")
+    private String appName;
+
+    @Value("${APP_ENV:default}")
+    private String appEnv;
+
+    @Value("${APP_VERSION:default}")
+    private String appVersion;
+
     @GetMapping("/")
     public String hello() throws UnknownHostException {
 
         contador++;
         String hostname = InetAddress.getLocalHost().getHostName();
 
-        return String.format("Versión 1 => Pod = %s , Visitas = %d  ", hostname, contador);
+        return String.format("Versión 1 => Pod = %s , Visitas = %d , Nombre = %s, Env=%s, Ver =%s "
+                , hostname, contador, appName, appEnv, appVersion);
     }
 
     /**
